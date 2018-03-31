e492ebad0e7cff3a0c7b869d8316396e24626613
smokris <smokris@softpixel.com>
2018-03-31 12:25:39 -0400

    Support building on Linux.

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -8,17 +8,17 @@ class DiscountTestConan(ConanFile):
         self.run('qbs -f "%s"' % self.source_folder)
 
     def imports(self):
-        self.copy('*',       src='bin', dst='bin')
-        self.copy('*.dylib', dst='lib', src='lib')
-        self.copy('*.so',    dst='lib', src='lib')
+        self.copy('*', src='bin', dst='bin')
+        self.copy('*', dst='lib', src='lib')
 
     def test(self):
-        self.run('qbs run')
+        self.run('qbs -f "%s"' % self.source_folder)
 
         # Ensure we only link to system libraries and our own libraries.
         if platform.system() == 'Darwin':
-            self.run('! (otool -L lib/*.dylib | grep -v "^lib/" | egrep -v "^\s*(/usr/lib/|/System/|@rpath/)")')
+            self.run('! (otool -L lib/libmarkdown.dylib | grep -v "^lib/" | egrep -v "^\s*(/usr/lib/|/System/|@rpath/)")')
+            self.run('! (otool -l lib/libmarkdown.dylib | grep -A2 LC_RPATH | cut -d"(" -f1 | grep "\s*path" | egrep -v "^\s*path @(executable|loader)_path")')
         elif platform.system() == 'Linux':
-            self.run('! (ldd lib/*.so | grep -v "^lib/" | grep "/" | egrep -v "\s/lib64/")')
+            self.run('! (ldd lib/libmarkdown.so | grep -v "^lib/" | grep "/" | egrep -v "\s/lib64/")')
         else:
             raise Exception('Unknown platform "%s"' % platform.system())
