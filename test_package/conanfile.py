8c458a0ba23176320a97473e2cd330246a1b15dc
Steve Mokris <smokris@softpixel.com>
2018-04-05 20:42:54 -0400

    Update to latest LLVM package and switch to libc++.

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -4,6 +4,8 @@ import platform
 class DiscountTestConan(ConanFile):
     generators = 'qbs'
 
+    requires = 'llvm/3.3-5@vuo/stable'
+
     def build(self):
         self.run('qbs -f "%s"' % self.source_folder)
 
@@ -17,8 +19,10 @@ class DiscountTestConan(ConanFile):
         # Ensure we only link to system libraries and our own libraries.
         if platform.system() == 'Darwin':
             self.run('! (otool -L lib/libmarkdown.dylib | grep -v "^lib/" | egrep -v "^\s*(/usr/lib/|/System/|@rpath/)")')
+            self.run('! (otool -L lib/libmarkdown.dylib | fgrep "libstdc++")')
             self.run('! (otool -l lib/libmarkdown.dylib | grep -A2 LC_RPATH | cut -d"(" -f1 | grep "\s*path" | egrep -v "^\s*path @(executable|loader)_path")')
         elif platform.system() == 'Linux':
             self.run('! (ldd lib/libmarkdown.so | grep -v "^lib/" | grep "/" | egrep -v "\s(/lib64/|(/usr)?/lib/x86_64-linux-gnu/)")')
+            self.run('! (ldd lib/libmarkdown.so | fgrep "libstdc++")')
         else:
             raise Exception('Unknown platform "%s"' % platform.system())
