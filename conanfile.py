bccac7894aa2c2a4951006f9fb8dce5d9db224fd
Steve Mokris <steve@kosada.com>
2020-10-31 22:11:46 -0400

    Update to discount 2.2.6 (latest).  Build for both x86_64 + arm64.  [kosada/vuo/vuo#17836]

--- conanfile.py
+++ conanfile.py
@@ -1,21 +1,26 @@
-from conans import ConanFile, tools
+from conans import ConanFile, CMake, tools
 import os
 import platform
 
 class DiscountConan(ConanFile):
     name = 'discount'
 
-    source_version = '2.2.3a'
-    package_version = '1'
+    source_version = '2.2.6'
+    package_version = '0'
     version = '%s-%s' % (source_version, package_version)
 
-    build_requires = 'llvm/3.3-5@vuo/stable'
+    build_requires = (
+        'llvm/5.0.2-1@vuo/stable',
+        'macos-sdk/11.0-0@vuo/stable',
+    )
     settings = 'os', 'compiler', 'build_type', 'arch'
     url = 'http://www.pell.portland.or.us/~orc/Code/markdown/'
     license = 'http://www.pell.portland.or.us/~orc/Code/markdown/COPYRIGHT.html'
     description = 'A library for formatting text into HTML'
     source_dir = 'discount-%s' % source_version
+    build_dir = '_build'
     install_dir = '_install'
+    exports_sources = '*.patch'
 
     def requirements(self):
         if platform.system() == 'Linux':
@@ -25,35 +30,32 @@ class DiscountConan(ConanFile):
 
     def source(self):
         tools.get('http://www.pell.portland.or.us/~orc/Code/markdown/discount-%s.tar.bz2' % self.source_version,
-                  sha256='75f5d5fda2e9607a5c77455296b41e5dd436389e20f35c37bea395d35aea0954')
+                  sha256='ae68a4832ff8e620286304ec525c1fe8957be4d8f1e774588eb03d1c3deb74a7')
+
+        tools.patch(patch_file='destructor.patch', base_path=self.source_dir)
 
         self.run('mv %s/COPYRIGHT %s/%s.txt' % (self.source_dir, self.source_dir, self.name))
 
     def build(self):
-        with tools.chdir(self.source_dir):
-            flags = '-Oz'
+        cmake = CMake(self)
 
-            if platform.system() == 'Darwin':
-                flags += ' -mmacosx-version-min=10.10'
+        cmake.definitions['CMAKE_BUILD_TYPE'] = 'Release'
+        cmake.definitions['CMAKE_C_COMPILER']   = '%s/bin/clang'   % self.deps_cpp_info['llvm'].rootpath
+        cmake.definitions['CMAKE_C_FLAGS'] = '-Oz'
+        cmake.definitions['CMAKE_INSTALL_NAME_DIR'] = '@rpath'
+        cmake.definitions['CMAKE_INSTALL_PREFIX'] = '%s/%s' % (os.getcwd(), self.install_dir)
+        cmake.definitions['CMAKE_OSX_ARCHITECTURES'] = 'x86_64;arm64'
+        cmake.definitions['CMAKE_OSX_DEPLOYMENT_TARGET'] = '10.11'
+        cmake.definitions['CMAKE_OSX_SYSROOT'] = self.deps_cpp_info['macos-sdk'].rootpath
+        cmake.definitions['BUILD_SHARED_LIBS'] = 'ON'
+        cmake.definitions['BUILD_STATIC_LIBS'] = 'OFF'
 
-            env_vars = {
-                'CC' : self.deps_cpp_info['llvm'].rootpath + '/bin/clang',
-                'CXX': self.deps_cpp_info['llvm'].rootpath + '/bin/clang++',
-                'CFLAGS': flags,
-            }
-            with tools.environment_append(env_vars):
-                self.run('./configure.sh --prefix=%s/../%s --shared --enable-all-features' % (os.getcwd(), self.install_dir))
-                if platform.system() == 'Linux':
-                    tools.replace_in_file('librarian.sh', '	test "/sbin/ldconfig" && /sbin/ldconfig "$1"', '')
-                self.run('make')
-                self.run('make install')
-
-        with tools.chdir(self.install_dir):
-            if platform.system() == 'Darwin':
-                self.run('install_name_tool -id @rpath/libmarkdown.dylib lib/libmarkdown.dylib')
-            elif platform.system() == 'Linux':
-                patchelf = self.deps_cpp_info['patchelf'].rootpath + '/bin/patchelf'
-                self.run('%s --set-soname libmarkdown.so lib/libmarkdown.so' % patchelf)
+        tools.mkdir(self.build_dir)
+        with tools.chdir(self.build_dir):
+            cmake.configure(source_dir='../%s/cmake' % self.source_dir,
+                            build_dir='.')
+            cmake.build()
+            cmake.install()
 
     def package(self):
         if platform.system() == 'Darwin':
